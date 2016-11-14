53bb3af1a5a9e485cced524560ced8c39a401cd7
orlando1108 <erwan.raulo2015@campus-eni.fr>
2016-11-14 15:44:16 +0100

    "edition addlayout"

---
+++ GestionnaireBibliotheque/AddBookLayout.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestionnaireBibliotheque
+{
+    /// <summary>
+    /// Logique d'interaction pour AddBookLayout.xaml
+    /// </summary>
+    public partial class AddBookLayout : Page
+    {
+        public AddBookLayout()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
